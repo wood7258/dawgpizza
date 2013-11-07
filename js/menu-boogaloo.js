594bedebb981becb5d3357225a55a5b300de54d2
wood7258 <wood7258@uw.edu>
2013-11-07 09:18:15 -0800

    version3.0

---
+++ js/menu-boogaloo.js
@@ -0,0 +1,43 @@
+// menu-boogaloo.js
+
+$(function() {
+	var idx;
+	var meatpies = $('.meat-list');
+	var vegpies = $('.veg-list');
+	var drinks = $('.drinks');
+	var desserts = $('.desserts');
+	var pizza;
+	var template = $('.template.pie');
+	for (idx = 0; idx < com.dawgpizza.menu.pizzas.length; idx++) {
+		pizza = com.dawgpizza.menu.pizzas[idx];
+		var menuline = template.clone();
+		menuline.find('.item-name').html(pizza.name);
+		menuline.find('.description').html(pizza.description);
+		menuline.find('.price').html('$' + pizza.prices[0] + '/' + '$' + pizza.prices[1] + '/' + '$' + pizza.prices[2]);
+        menuline.removeClass('template');
+		if (pizza.vegetarian) {
+			vegpies.append(menuline);
+		}
+		else {
+			meatpies.append(menuline);
+		}
+	}
+	var drink;
+	var template = $('.template.dedr');
+	for (idx = 0; idx < com.dawgpizza.menu.drinks.length; ++idx) {
+		drink = com.dawgpizza.menu.drinks[idx];
+		var menuline = template.clone();
+		menuline.find('.item-name').html(drink.name);
+		menuline.find('.price').html('$' + drink.price);
+        menuline.removeClass('template');
+		drinks.append(menuline);
+	}
+	for (idx = 0; idx < com.dawgpizza.menu.desserts.length; ++idx) {
+		drink = com.dawgpizza.menu.desserts[idx];
+		var menuline = template.clone();
+		menuline.find('.item-name').html(drink.name);
+		menuline.find('.price').html('$' + drink.price);
+        menuline.removeClass('template');
+		desserts.append(menuline);
+	}
+});
