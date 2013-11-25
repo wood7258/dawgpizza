f3173aaf9d894d8aa214f4e690e7da8292170d4c
wood7258 <wood7258@uw.edu>
2013-11-25 09:45:16 -0800

    fixed pricing on menu page

--- js/menu-boogaloo.js
+++ js/menu-boogaloo.js
@@ -13,7 +13,7 @@ $(function() {
 		var menuline = template.clone();
 		menuline.find('.item-name').html(pizza.name);
 		menuline.find('.description').html(pizza.description);
-		menuline.find('.price').html('$' + pizza.prices[0] + '/' + '$' + pizza.prices[1] + '/' + '$' + pizza.prices[2]);
+		menuline.find('.menu-price').html('$' + pizza.prices[0] + '/' + '$' + pizza.prices[1] + '/' + '$' + pizza.prices[2]);
         menuline.removeClass('template');
 		if (pizza.vegetarian) {
 			vegpies.append(menuline);
@@ -28,7 +28,7 @@ $(function() {
 		drink = com.dawgpizza.menu.drinks[idx];
 		var menuline = template.clone();
 		menuline.find('.item-name').html(drink.name);
-		menuline.find('.price').html('$' + drink.price);
+		menuline.find('.menu-price').html('$' + drink.price);
         menuline.removeClass('template');
 		drinks.append(menuline);
 	}
@@ -36,7 +36,7 @@ $(function() {
 		drink = com.dawgpizza.menu.desserts[idx];
 		var menuline = template.clone();
 		menuline.find('.item-name').html(drink.name);
-		menuline.find('.price').html('$' + drink.price);
+		menuline.find('.menu-price').html('$' + drink.price);
         menuline.removeClass('template');
 		desserts.append(menuline);
 	}
